e9fa3f5cce46cf5fba65fb80ae3148da692c44ce
Pratheesh Satheesh Kumar <116820443+pratheeshsatheeshkumar@users.noreply.github.com>
2024-01-24 23:03:01 +0530

    Update provider.tf
    removed credentials

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA5FTZFHFS22VEBK42"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
 #But I should check the possibility of using IAM roles instead
