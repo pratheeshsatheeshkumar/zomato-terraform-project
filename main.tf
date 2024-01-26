bfa2f81503249745339bede8356f1d011d03e067
pratheesh <yespratheesh@gmail.com>
2024-01-26 11:53:16 +0530

    created aws_key_pair and variables

--- main.tf
+++ main.tf
@@ -1,13 +1,24 @@
-resource "aws_instance" "zomato-frontend" {
-  ami                    = "ami-0c84181f02b974bc3"
-  associate_public_ip_address = true
-  subnet_id = "subnet-06215c494549b1531"
-  instance_type          = "t2.micro"
-  key_name               = "yespratheeshdevops"
-  vpc_security_group_ids = ["sg-0406f3931b3187f8c"]
+resource "aws_key_pair" "aws-keypair" {
+  key_name   = "zomato-prod"
+  public_key = file("/home/ubuntu/keys/aws_key.pub")
   tags = {
-    "Name"    = "zomato-frontend"
-    "project" = "zomato"
-    "env"     = "prod"
+    "Name"    = "zomato-aws-keypair"
+    "project" = var.project
+    "env"     = var.env
   }
 }
+
+resource "aws_instance" "zomato-frontend" {
+  ami                         = "ami-0c84181f02b974bc3"
+  associate_public_ip_address = true
+  subnet_id                   = "subnet-06215c494549b1531"
+  instance_type               = "t2.micro"
+  key_name                    = "zomato-prod"
+  vpc_security_group_ids      = ["sg-0406f3931b3187f8c"]
+  tags = {
+    "Name"    = "zomato-frontend"
+    "project" = var.project
+    "env"     = var.env
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "project" {
+  default     = "zomato"
+  description = "name of the project"
+
+}
+
+variable "env" {
+  default     = "prod"
+  description = "environment of the project"
+
+}
+
+variable "region" {
+  default     = "ap-south-1"
+  description = "aws region"
+}
